674b058bf325d71f49887286c63e1575e26705fc
spirit-animal <54417528+spirit-animal@users.noreply.github.com>
2023-11-28 17:40:42 +0900

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -30,19 +30,9 @@ const SendMessage = user => {
   }
 };
 const GenerateMessage = name => {
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  const d = new Date();
-  const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
-  // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
+  
+  return `Welcome to my domain 😈 `; // your message
+  // My message  
 };
 
 module.exports = AutoDM;
